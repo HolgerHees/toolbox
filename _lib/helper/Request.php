4219059c717f6628eb0c0a98dd531b29a3594cb1
Holger Hees <holger.hees@gmail.com>
2019-05-16 12:25:32 +0200

    fix rest request class

--- _lib/helper/Request.php
+++ _lib/helper/Request.php
@@ -8,14 +8,11 @@ class Request
         $ch = curl_init();
         curl_setopt( $ch, CURLOPT_URL, $url );
         curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-        if( $postData != null )
+        
+        if( $postData !== null )
         {
             curl_setopt( $ch, CURLOPT_POST, 1 );
-
-            if( !empty( $postData ) )
-            {
-                curl_setopt( $ch, CURLOPT_POSTFIELDS, $postData );
-            }
+            curl_setopt( $ch, CURLOPT_POSTFIELDS, $postData );
         }
 
         curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
