7e6b701e81fe51fc8c19774c0a214e7f7bc5fbe2
Holger Hees <holger.hees@gmail.com>
2019-07-18 20:15:39 +0200

    small css changes to weather widget html

--- _lib/openhab/Weather.php
+++ _lib/openhab/Weather.php
@@ -203,7 +203,7 @@ class Weather
 
 			$typeTopOffset = isset( Weather::$sunConfig['typeIconOffsetTop'][$type][$icon] ) ? Weather::$sunConfig['typeIconOffsetTop'][$type][$icon] : Weather::$sunConfig['typeIconOffsetTop'][$type]['default'];
 
-            $svg .= '<div class="'.$type.'_background_' . $amount . '" style="left:'.$typeLeftOffset.'%;top:'.$typeTopOffset.'%"></div>';
+            $svg .= '<div class="'.$type.'_background_' . $amount . ' raindrop_snowflake_background" style="left:'.$typeLeftOffset.'%;top:'.$typeTopOffset.'%"></div>';
 			$svg .= '<svg style="left:'.$typeLeftOffset.'%;top:'.$typeTopOffset.'%" class="'.$type.'" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 64 64">
                     <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/static/theme/svg/icons.svg#self_'.$type.$amount.'_grayscaled" />
                  </svg>';
